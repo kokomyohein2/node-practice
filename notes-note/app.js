9ef282728123a38830c8f35910596f94bf1dc0ad
KoKoMyoHeinHtet <komyo2000@gmail.com>
2018-09-11 07:58:43 +0700

    019 : read note

--- notes-note/app.js
+++ notes-note/app.js
@@ -18,8 +18,7 @@ if (command === 'add'){
   if (note){
     console.log('Note created');
     console.log('---');
-    console.log(`Title : ${note.title}`);
-    console.log(`Body : ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log('Note title taken');
   }
@@ -27,7 +26,13 @@ if (command === 'add'){
   console.log('Listing all notes');
   notes.getAll();
 } else if(command === 'read'){
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note Found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
 }else if (command === 'remove'){
   var noteRemoved = notes.removeNote(argv.title);
   var message = noteRemoved ? 'Note was removed' : 'Note not found';
