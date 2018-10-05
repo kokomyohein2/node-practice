6dd066eebcdf9b1f1b074b912848300c59cef5ee
KoKoMyoHeinHtet(Ko-Dell) <komyo2000@gmail.com>
2018-10-05 07:46:09 +0700

    030 : Added JSON.stringify

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -4,5 +4,6 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(response, undefined, 2))
+    console.log(`Address: ${body.status}`)
 });
