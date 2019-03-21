bfcf207d627c716d3d5a500aab9d2e101354adf4
Ko-PN <komyo2000@gmail.com>
2019-03-21 20:06:11 +0700

    034 : Wiring Up Weather Search - call to weather forecast - console developer.forecast.io

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,29 +1,46 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode')
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        },
-        key: {
-            demand: true,
-            alias: 'apiKey',
-            describe: 'Google Maps API key',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const geocode = require('./geocode/geocode')
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         },
+//         key: {
+//             demand: true,
+//             alias: 'apiKey',
+//             describe: 'Google Maps API key',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.a, argv.key, (errorMessage, results) => {
-if(errorMessage){
-    console.log(errorMessage);
-}else{
-    console.log(JSON.stringify(results,undefined,2));
-}
+// geocode.geocodeAddress(argv.a, argv.key, (errorMessage, results) => {
+// if(errorMessage){
+//     console.log(errorMessage);
+// }else{
+//     console.log(JSON.stringify(results,undefined,2));
+// }
+// });
+
+// 391f89f9e2cdf219fa000e239f873286
+// https://api.darksky.net/forecast/391f89f9e2cdf219fa000e239f873286/37.8267,-122.4233
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/391f89f9e2cdf219fa000e239f873286/13.75398,100.50144?units=si',
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch weather.');
+        console.log(error);
+    }
 });
