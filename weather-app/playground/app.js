eff1e87e85cc06b09e626ac62b51a1d7ff94d71e
Ko-PN <komyo2000@gmail.com>
2019-03-21 13:01:20 +0700

    031 : Encoding User Input and use as dynamic api call

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,9 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    // console.log(JSON.stringify(response, undefined, 2))
-    console.log(`Address: ${body.status}`)
+    console.log(`Address: ${body.result[0].formatted_address}`);
+    console.log(`Latitude: ${body.result[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.result[0].geometry.location.lng}`);
 });
