8fbd7005befd4ca39b6c2ce3be2f9547377a939d
Ko-PN <komyo2000@gmail.com>
2019-03-21 18:13:24 +0700

    032 : Call Back -added argument apikey

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -8,6 +8,12 @@ const argv = yargs
             alias: 'address',
             describe: 'Address to fetch weather for',
             string: true
+        },
+        key: {
+            demand: true,
+            alias: 'API Key',
+            describe: 'Google Maps API key',
+            string: true
         }
     })
     .help()
@@ -15,12 +21,19 @@ const argv = yargs
     .argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
+var apiKey = argv.key;
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.result[0].formatted_address}`);
-    console.log(`Latitude: ${body.result[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.result[0].geometry.location.lng}`);
+    if (error) {
+        console.log("Unable to connect");
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.result[0].formatted_address}`);
+        console.log(`Latitude: ${body.result[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.result[0].geometry.location.lng}`);
+    } else {
+        console.log(body.status);
+    }
 });
