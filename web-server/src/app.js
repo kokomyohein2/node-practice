34156a2c449b4d9e711893513e0b16631f5119d7
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2020-07-30 23:07:21 +0700

    047 : Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'KoKo'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'KoKo'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        text: 'This is sample text'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send('Weather  Page');
