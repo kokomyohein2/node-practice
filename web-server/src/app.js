eeb6a9140179f1e16b1b21dc0e43e595b737b239
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2020-07-31 08:28:57 +0700

    052 : Styling the Application: Part II

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath));
+hbs.registerPartials(partialsPath);
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -24,12 +29,27 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
+        name: 'KoKo',
         text: 'This is sample text'
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+    title: '404',
+    name: 'KoKo',
+    errorMessage:'Help article not found.'})
+});
+
 app.get('/weather', (req, res) => {
     res.send('Weather  Page');
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+    title: '404',
+    name: 'KoKo',
+    errorMessage:'It is not found.'})
 });
 
 app.listen(3000, () => {
