1cc5cad0bb2fc8544e04eff1e618488415dd0cff
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-11 11:44:01 +0700

    076 : Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -11,8 +11,54 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+    //
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'KoKo',
+    //         age: 25
+    //     },
+    //     {
+    //         name: 'nge',
+    //         age: 24
+    //     },
+    //     {
+    //         name: 'booboo',
+    //         age: 5
+    //     }], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert users');
+    //     }
+    //
+    //     console.log(result.ops);
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        },{
+            description: 'Renew inspection',
+            completed: false
+        },{
+            description: 'Pot plants',
+            completed: false
+        }
+        ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks');
+        }
+
+        console.log(result.ops);
+    });
+
 })
