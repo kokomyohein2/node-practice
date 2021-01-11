eb73ede780d4daa3bdaf48ae84046f8fb34ec6a0
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-11 14:46:17 +0700

    077 : The ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,16 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const MongoObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if (error) {
@@ -12,8 +20,9 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     const db = client.db(databaseName);
 
     // db.collection('users').insertOne({
-    //     name: 'Andrew',
-    //     age: 27
+    //     _id: id,
+    //     name: 'Vikram',
+    //     age: 26
     // }, (error, result) => {
     //     if (error) {
     //         return console.log('Unable to insert user');
@@ -42,23 +51,23 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     //     console.log(result.ops);
     // });
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Clean the house',
-            completed: true
-        },{
-            description: 'Renew inspection',
-            completed: false
-        },{
-            description: 'Pot plants',
-            completed: false
-        }
-        ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert tasks');
-        }
-
-        console.log(result.ops);
-    });
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the house',
+    //         completed: true
+    //     },{
+    //         description: 'Renew inspection',
+    //         completed: false
+    //     },{
+    //         description: 'Pot plants',
+    //         completed: false
+    //     }
+    //     ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks');
+    //     }
+    //
+    //     console.log(result.ops);
+    // });
 
 })
