f1931e37b388591033a87609d725d4a3a8898d37
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-22 09:03:44 +0700

    086 : Data Validation and Sanitization: Part II

--- task-manager/src/mongodb.js
+++ task-manager/src/mongodb.js
@@ -7,62 +7,76 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology: true //for future support
 })
 
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minLength: 7,
+//         trim: true,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('password cannot contain "password"')
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be positive')
+//             }
+//         }
+//     }
+// })
+//
+// const me = new User({
+//     name: '    Andrew    ',
+//     email: 'MIKE@ME.IO    ',
+//     password:'Pass090!@#'
+// })
+//
+// me.save().then(() => {
+//     console.log(me)
+// }).catch(e => {
+//     console.log('Error!', e)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
         required: true,
         trim: true
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive')
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false
     }
 })
 
-const me = new User({
-    name: '    Andrew    ',
-    email: 'MIKE@ME.IO    '
+const task = new Task({
+    description: '     shower booboo   '
 })
 
-me.save().then(() => {
-    console.log(me)
+
+task.save().then(() => {
+    console.log(task)
 }).catch(e => {
     console.log('Error!', e)
 })
-
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
-//
-// const task = new Task({
-//     description: 'shower booboo',
-//     completed: false
-// })
-//
-//
-// task.save().then(() => {
-//     console.log(task)
-// }).catch(e => {
-//     console.log('Error!', e)
-// })
