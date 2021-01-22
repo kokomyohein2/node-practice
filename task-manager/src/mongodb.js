eaef83e5bc4fb93d4edda248ccac6d9dd9d18d7d
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-22 08:19:58 +0700

    086 : Data Validation and Sanitization: Part I

--- task-manager/src/mongodb.js
+++ task-manager/src/mongodb.js
@@ -1,48 +1,68 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    // useUnifiedTopology: true //for future support
+    useUnifiedTopology: true //for future support
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-//
-// const me = new User({
-//     name: 'Andrew',
-//     age: 27
-// })
-//
-// me.save().then(() => {
-//     console.log(me)
-// }).catch(e => {
-//     console.log('Error!', e)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    completed: {
-        type: Boolean
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive')
+            }
+        }
     }
 })
 
-const task = new Task({
-    description: 'shower booboo',
-    completed: false
+const me = new User({
+    name: '    Andrew    ',
+    email: 'MIKE@ME.IO    '
 })
 
-
-task.save().then(() => {
-    console.log(task)
+me.save().then(() => {
+    console.log(me)
 }).catch(e => {
     console.log('Error!', e)
 })
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+//
+// const task = new Task({
+//     description: 'shower booboo',
+//     completed: false
+// })
+//
+//
+// task.save().then(() => {
+//     console.log(task)
+// }).catch(e => {
+//     console.log('Error!', e)
+// })
