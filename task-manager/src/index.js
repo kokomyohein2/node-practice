37c8ad5d9b1c8258d9feb5bd3af91272c5f6c480
Kokomyoheinhtet Unknown <kokomyoheinhtet@ascendcorp.com>
2021-01-28 08:21:55 +0700

    092 : Resource Reading Endpoints: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -20,6 +20,27 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch(e => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    User.findById(id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch(e => {
+        res.status(500).send(e)
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -27,6 +48,28 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch(e => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch(e => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    Task.findById(id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch(e => {
+        res.status(500).send(e)
     })
 })
 
